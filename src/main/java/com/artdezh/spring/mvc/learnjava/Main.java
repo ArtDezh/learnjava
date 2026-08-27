d5b6ec5824d85aebc83ddbfc3b7a93e6af6774a9
ArtDezh <artur2134@mail.ru>
2026-08-27 21:25:08 +0300

    Наследование финал

--- src/main/java/com/artdezh/spring/mvc/learnjava/Main.java
+++ src/main/java/com/artdezh/spring/mvc/learnjava/Main.java
@@ -3,8 +3,13 @@ package com.artdezh.spring.mvc.learnjava;
 public class Main {
     static void main() {
         Student student1 = new Student("Peter", 25, "МГУ");
+        Teacher teacher = new Teacher("Анна", 22, "математику");
 
         student1.introduce();
         student1.study();
+
+        System.out.println("--------------------------------");
+
+        teacher.teach();
     }
 }
