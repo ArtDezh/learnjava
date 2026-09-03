1db60cbee2cbe56aae63f5619db3a9467ccc32eb
ArtDezh <artur2134@mail.ru>
2026-09-03 18:40:53 +0300

    Finish

--- src/main/java/com/artdezh/spring/mvc/learnjava/fibonacci/Main.java
+++ src/main/java/com/artdezh/spring/mvc/learnjava/fibonacci/Main.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = readInput();
-        List<Long> result = new ArrayList<>();
+        List<Long> result = printFibonacci(n);
+        System.out.println(result);
 
     }
 
@@ -16,4 +17,19 @@ public class Main {
         return Integer.parseInt(scanner.nextLine().trim());
     }
 
+    public static List<Long> printFibonacci(int count) {
+        int first = 0;
+        int second = 1;
+        List<Long> listResult = new ArrayList<>();
+        listResult.addFirst(0L);
+        for (int i = 1; i < count; i++) {
+            //System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+            listResult.add((long) first);
+        }
+        return listResult;
+    }
 }
